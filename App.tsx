de6a42baad678ab9a29bd0a0f023980674eb53ff
suportedeluxejob-arch <suporte.deluxejob@gmail.com>
2026-01-13 20:28:53 -0300

    fies

--- App.tsx
+++ App.tsx
@@ -19,7 +19,7 @@ function App() {
             <img
               src={LOGO_URL}
               alt="WG PATCH Logo"
-              className="h-25 w-25 object-cover rounded-full shadow-lg border-2 border-white mb-2"
+              className="h-19 w-19 object-cover rounded-full shadow-lg border-2 border-white mb-2"
             />
             <div className="text-xl font-black text-slate-900 tracking-tighter">
               WG<span className="text-cyan-500"> PATCH</span>
